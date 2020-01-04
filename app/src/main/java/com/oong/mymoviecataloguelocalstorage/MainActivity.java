67ce78c92b9641e309df3cbfafbbf38473cecaff
Oong <mkhalil26122000@gmail.com>
2020-01-04 13:17:28 +0700

    Fix

--- app/src/main/java/com/oong/mymoviecataloguelocalstorage/MainActivity.java
+++ app/src/main/java/com/oong/mymoviecataloguelocalstorage/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Inisial
         init();
     }
 
