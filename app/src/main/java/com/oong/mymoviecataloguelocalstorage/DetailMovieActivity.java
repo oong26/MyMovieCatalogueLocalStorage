7884b8ced38d5dc0eb032683daf768904feda503
Oong <mkhalil26122000@gmail.com>
2020-01-04 14:13:17 +0700

    Fix Show Movies & TV show

---
+++ app/src/main/java/com/oong/mymoviecataloguelocalstorage/DetailMovieActivity.java
@@ -0,0 +1,14 @@
+package com.oong.mymoviecataloguelocalstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
